ca604db7b906a2d8a4caabefa36efcf12fc1ffe8
Halil CAN <halilc.2001@gmail.com>
2022-02-26 23:25:01 +0300

    finished user-actions tests

--- tests/v1-api/user-actions.test.ts
+++ tests/v1-api/user-actions.test.ts
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
 import request from 'supertest';
 import faker from "@faker-js/faker";
+import bcrypt from "bcrypt";
 
 import app from "../../src/app";
 import User from '../../src/models/user';
 import UserRegisterCode from "../../src/models/user-register-code";
+import UserResetPassword from "../../src/models/user-reset-password";
 
-
+//{ useNewUrlParser: true, useUnifiedTopology: true }
 beforeEach(async () => {
     //todo:app içinde mongofb bağlantısı zaten yapıyor ?
     await mongoose.connect('mongodb://localhost:27017/key-manager');
@@ -46,8 +48,7 @@ describe('USER ACTIONS API', () => {
             password: 'password'
         };
 
-        //todo: bunun yerine api kullanmak ? çünkü password gibi şeylerde bcrypt yapmak gerekiyor.
-        await User.create(user);
+        await User.create({ ...user, password: await bcrypt.hash(user.password, 10) });
 
         await request(app)
             .post('/api/v1/user-actions/login')
@@ -55,5 +56,58 @@ describe('USER ACTIONS API', () => {
             .expect(200);
     });
 
+    test('/reset-password', async () => {
+        const user = {
+            name: 'name',
+            surname: 'surname',
+            email: faker.internet.email(),
+            password: 'password'
+        };
 
+        await User.create({ ...user, password: await bcrypt.hash(user.password, 10) });
+
+        await request(app)
+            .post('/api/v1/user-actions/reset-password')
+            .send({ email: user.email })
+            .expect(201);
+    });
+
+    test('/reset-password/:key', async () => {
+        const user = {
+            name: 'name',
+            surname: 'surname',
+            email: faker.internet.email(),
+            password: 'password'
+        };
+        const resetPasswordUser = {
+            email: user.email,
+            key: 123456
+        };
+
+        await User.create({ ...user, password: await bcrypt.hash(user.password, 10) });
+        await UserResetPassword.create(resetPasswordUser);
+
+        await request(app)
+            .post(`/api/v1/user-actions/reset-password/${resetPasswordUser.key}`)
+            .send({ email: user.email, password: 'newPassword' })
+            .expect(200);
+    });
+
+    test('/logout', async () => {
+        const user = {
+            name: 'name',
+            surname: 'surname',
+            email: faker.internet.email(),
+            password: 'password'
+        };
+        const testToken = { name: 'test', token: 'test-token' };
+
+        const createdUser = await User.create({ ...user, password: await bcrypt.hash(user.password, 10) });
+        await User.findOneAndUpdate({ _id: createdUser._id }, { tokens: [testToken] });
+
+        await request(app)
+            .post('/api/v1/user-actions/logout')
+            .set('Authorization', testToken.token)
+            .expect(200);
+    });
 });
